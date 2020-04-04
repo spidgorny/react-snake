91122ff45a7c8b93b37c3164d67dd80fb0bf077d
DEPIDSVY <depidsvy@nintendo.de>
2020-04-04 13:34:10 +0200

    - add rendering of the grid and snake head

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,77 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Point {
+  x: number;
+  y: number;
+
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+
+  equals({x, y}: {x: number, y: number}) {
+    return this.x === x && this.y === y;
+  }
+
+}
+
+interface IAppState {
+  grid: string[][];
+  head: Point;
+}
+
+class App extends React.Component {
+
+  readonly width = 30;
+  readonly height = 30;
+
+  state: IAppState = {
+    grid: [],
+    head: new Point(0, 0),
+  }
+
+  constructor(props: any) {
+    super(props);
+    // @ts-ignore
+    this.state.grid = Array(this.height).fill(null).map(el => {
+      return Array(this.width).fill(null);
+    });
+    // console.log(this.state.grid);
+    // @ts-ignore
+    window['app'] = this;
+    this.state.head = new Point(Math.round(this.width / 2), this.height - 1);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.renderGrid()}
+      </div>
+    );
+  }
+
+  renderGrid() {
+    return (
+      <table className="grid">
+        <tbody>
+        {this.state.grid.map((row, y) =>
+          <tr key={y}>
+            {row.map((cell, x) => {
+                let cellType = this.state.grid[y][x];
+                if (this.state.head.equals({x, y})) {
+                  cellType = 'head';
+                }
+                return <td key={x} className={cellType}/>;
+              }
+            )}
+          </tr>
+        )}
+        </tbody>
+      </table>
+    )
+  }
+
 }
 
 export default App;
